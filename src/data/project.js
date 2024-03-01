8c36663ce7389ef413aa4756f189699d87896205
sajiwaniDewi <dewisanjiw@gmail.com>
2024-03-01 15:32:58 +0900

    add dummy data

---
+++ src/data/project.js
@@ -0,0 +1,12 @@
+const projects = [
+    {
+        id: "9489805f-c57d-4485-a36a-810f4d2fc50f",
+        name: "xasxa",
+        team: "Kambing",
+        start: "03/1/2024",
+        end: "13/03/2024",
+        detail: "xsaxa",
+    },
+];
+
+export default projects;
